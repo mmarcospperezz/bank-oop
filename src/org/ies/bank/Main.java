7abfa38847a4a673ad299b2c44968642660c5097
mmarcospperezz <mmarcospperez2204@gmail.com>
2024-12-03 12:15:39 +0100

    bankapp

--- src/org/ies/bank/Main.java
+++ src/org/ies/bank/Main.java
@@ -2,6 +2,5 @@ package org.ies.bank;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
--- src/org/ies/bank/components/BankApp.java
+++ src/org/ies/bank/components/BankApp.java
@@ -7,11 +7,30 @@ import java.util.Scanner;
 public class BankApp {
 
     public final Scanner scanner;
-    public final Bank bank;
+    public final BankReader bankReader;
 
-    public BankApp(Scanner scanner, Bank bank) {
+    public BankApp(Scanner scanner, BankReader bankReader) {
         this.scanner = scanner;
-        this.bank = bank;
+        this.bankReader = bankReader;
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        Bank banco = bankReader.read();
+
+        int opcion;
+        do {
+            System.out.println("--- Menú ---");
+            System.out.println("1. Mostrar todas las cuentas");
+            System.out.println("2. Mostrar datos de una cuenta por IBAN");
+            System.out.println("3. Mostrar cuentas de un cliente por NIF");
+            System.out.println("4. Ingresar dinero en una cuenta");
+            System.out.println("5. Retirar dinero de una cuenta");
+            System.out.println("0. Salir");
+            System.out.print("Selecciona una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+        }
+        while (opcion != 0);
     }
 }
-
